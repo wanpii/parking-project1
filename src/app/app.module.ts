54b804355f31ee8e5a675f997e3a3f635ed10743
Jevgenijs Jaunslavietis <jevgenijs.jaunslavietis@tadano.com>
2022-11-09 17:13:19 +0200

    Generating components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPageComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule
